48a4cb8161bc6d87a051771bfc38edc4b3bef901
Cristina Plesa <cristinaplesa8@gmail.com>
2021-07-23 11:49:06 -0700

    WIP: Read built in methods from canvas to simplify Pythagoras Tree function

--- js/script.js
+++ js/script.js
@@ -2,39 +2,39 @@ $(document).ready(function () {
   const c = document.getElementById("canvas_is_cool");
   const ctx = c.getContext("2d"); // context variable is used to draw on a 2D plane
 
-  // const createTriangle = (pos, sidelen) => {
-  //   ctx.beginPath();
-  //   ctx.moveTo(...pos); // go to the left vertex
+  const createTriangle = (pos, sidelen) => {
+    ctx.beginPath();
+    ctx.moveTo(...pos); // go to the left vertex
 
-  //   // (0, 0) is the top-left corner of the canvas
-  //   // (0, 100) renders 100px down from the top
-  //   // to move back "up" on the screen from (0, 100) you would have to subtract
-  //   ctx.lineTo(pos[0] + sidelen / 2, pos[1] - sidelen * Math.sin(Math.PI/3)); // draw line from left vertex to top vertex
-  //   ctx.lineTo(pos[0] + sidelen, pos[1]); // draw line from top vertex to right vertex
-  //   ctx.lineTo(...pos); // draw line from right vertex back to left vertex
-  //   ctx.closePath();
-  //   ctx.fill(); // fills the most recently closed path: this case being the triangle
-  // }
-  // createTriangle([1, 100], 50);
+    // (0, 0) is the top-left corner of the canvas
+    // (0, 100) renders 100px down from the top
+    // to move back "up" on the screen from (0, 100) you would have to subtract
+    ctx.lineTo(pos[0] + sidelen / 2, pos[1] - sidelen * Math.sin(Math.PI/3)); // draw line from left vertex to top vertex
+    ctx.lineTo(pos[0] + sidelen, pos[1]); // draw line from top vertex to right vertex
+    ctx.lineTo(...pos); // draw line from right vertex back to left vertex
+    ctx.closePath();
+    ctx.fill(); // fills the most recently closed path: this case being the triangle
+  }
+  createTriangle([1, 100], 50);
   
-  // const createSierpinskiTriangle = (pos, sidelen, depth) => {
-  //   const innerTriangleSidelen = sidelen / 2; // side length of inner triangles is half the side length of the outer triangle
-  //   const innerTrianglePositions = [
-  //     pos,
-  //     [ pos[0] + innerTriangleSidelen, pos[1] ],
-  //     [ pos[0] + innerTriangleSidelen / 2, pos[1] - Math.sin(Math.PI/3) * innerTriangleSidelen ]
-  //   ]; // these positions are the same as what was used in the createTriangle function
-  //   if(depth == 0) {
-  //     innerTrianglePositions.forEach((trianglePosition) => {
-  //       createTriangle(trianglePosition, innerTriangleSidelen);
-  //     });
-  //   } else {
-  //     innerTrianglePositions.forEach((trianglePosition) => {
-  //       createSierpinskiTriangle(trianglePosition, innerTriangleSidelen, depth - 1);
-  //     });
-  //   }
-  // }
-  // createSierpinskiTriangle([0, 1000], 1000, 6);
+  const createSierpinskiTriangle = (pos, sidelen, depth) => {
+    const innerTriangleSidelen = sidelen / 2; // side length of inner triangles is half the side length of the outer triangle
+    const innerTrianglePositions = [
+      pos,
+      [ pos[0] + innerTriangleSidelen, pos[1] ],
+      [ pos[0] + innerTriangleSidelen / 2, pos[1] - Math.sin(Math.PI/3) * innerTriangleSidelen ]
+    ]; // these positions are the same as what was used in the createTriangle function
+    if(depth == 0) {
+      innerTrianglePositions.forEach((trianglePosition) => {
+        createTriangle(trianglePosition, innerTriangleSidelen);
+      });
+    } else {
+      innerTrianglePositions.forEach((trianglePosition) => {
+        createSierpinskiTriangle(trianglePosition, innerTriangleSidelen, depth - 1);
+      });
+    }
+  }
+  createSierpinskiTriangle([0, 1000], 1000, 6);
 
   const createSquare = (pos, sidelen) => {
     ctx.beginPath();
@@ -46,12 +46,14 @@ $(document).ready(function () {
     ctx.closePath();
     ctx.fill();
   }
-  createSquare([1, 100], 50);
+  createSquare([100, 100], 50);
 
   const createPythagorasTree = (pos, sidelen, depth) => {
     const smallerSquareSidelen = sidelen / (Math.sqrt(2) / 2);
 
-    
+    const leftSquare = [
+      
+    ]
   }
 
   const downloadCanvasContent = () => {
